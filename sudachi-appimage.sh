53b429d344669631d4330dc8d236fd37db470bb0
pflyly <81011361+pflyly@users.noreply.github.com>
2025-03-29 21:33:38 +0100

    Update sudachi-appimage.sh

--- sudachi-appimage.sh
+++ sudachi-appimage.sh
@@ -25,65 +25,7 @@ if [ ! -d ./sudachi ]; then
 fi
 
 cd ./sudachi
-
-# List of submodule paths
-submodule_paths=(
-    "externals/enet"
-    "externals/dynarmic"
-    "externals/libusb/libusb"
-    "externals/discord-rpc"
-    "externals/vulkan-headers"
-    "externals/sirit"
-    "externals/mbedtls"
-    "externals/xbyak"
-    "externals/opus"
-    "externals/cpp-httplib"
-    "externals/ffmpeg/ffmpeg"
-    "externals/cpp-jwt"
-    "externals/libadrenotools"
-    "externals/VulkanMemoryAllocator"
-    "externals/breakpad"
-    "externals/simpleini"
-    "externals/oaknut"
-    "externals/Vulkan-Utility-Libraries"
-    "externals/vcpkg"
-    "externals/nx_tzdb/tzdb_to_nx"
-    "externals/cubeb"
-    "externals/SDL3"
-)
-
-for path in "${submodule_paths[@]}"; do
-    if [ -d "$path" ]; then
-        echo "Deleting existing folder: $path"
-        rm -rf "$path"
-    fi
-done
-
 git init
-
-git submodule add https://github.com/lsalzman/enet externals/enet
-git submodule add https://github.com/sudachi-emu/dynarmic externals/dynarmic
-git submodule add https://github.com/libusb/libusb externals/libusb/libusb
-git submodule add https://github.com/sudachi-emu/discord-rpc externals/discord-rpc
-git submodule add https://github.com/KhronosGroup/Vulkan-Headers externals/vulkan-headers
-git submodule add https://github.com/sudachi-emu/sirit externals/sirit
-git submodule add https://github.com/sudachi-emu/mbedtls externals/mbedtls
-git submodule add https://github.com/herumi/xbyak externals/xbyak
-git submodule add https://github.com/xiph/opus externals/opus
-git submodule add https://github.com/yhirose/cpp-httplib externals/cpp-httplib
-git submodule add https://github.com/FFmpeg/FFmpeg externals/ffmpeg/ffmpeg
-git submodule add https://github.com/arun11299/cpp-jwt externals/cpp-jwt
-git submodule add https://github.com/bylaws/libadrenotools externals/libadrenotools
-git submodule add https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator externals/VulkanMemoryAllocator
-git submodule add https://github.com/sudachi-emu/breakpad externals/breakpad
-git submodule add https://github.com/brofield/simpleini externals/simpleini
-git submodule add https://github.com/sudachi-emu/oaknut externals/oaknut
-git submodule add https://github.com/KhronosGroup/Vulkan-Utility-Libraries externals/Vulkan-Utility-Libraries
-git submodule add https://github.com/microsoft/vcpkg externals/vcpkg
-git submodule add https://github.com/lat9nq/tzdb_to_nx externals/nx_tzdb/tzdb_to_nx
-git submodule add https://github.com/mozilla/cubeb externals/cubeb
-git submodule add https://github.com/libsdl-org/sdl externals/SDL3
-
 git submodule update --init --recursive
 
 #Replaces 'boost::asio::io_service' with 'boost::asio::io_context' for compatibility with Boost.ASIO versions 1.74.0 and later
