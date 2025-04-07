7eb4b5a3fcdedc34f0356eb967d2485a1a278fad
Escary <81011361+pflyly@users.noreply.github.com>
2025-04-07 11:59:48 +0100

    update for upstream change (#5)
    * change the appimage build name
    
    * workaround of cmake error
    
    * prefer this naming rule

--- torzu-appimage.sh
+++ torzu-appimage.sh
@@ -5,10 +5,7 @@ set -e
 export APPIMAGE_EXTRACT_AND_RUN=1
 export ARCH="$(uname -m)"
 LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
-URUNTIME=$(wget -q https://api.github.com/repos/VHSgunzo/uruntime/releases -O - \
-	| sed 's/[()",{} ]/\n/g' | grep -oi "https.*appimage.*dwarfs.*$ARCH$" | head -1)
-ICON="https://notabug.org/litucks/torzu/raw/02cfee3f184e6fdcc3b483ef399fb5d2bb1e8ec7/dist/yuzu.png"
-ICON_BACKUP="https://free-git.org/Emulator-Archive/torzu/raw/branch/master/dist/yuzu.png"
+URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
 
 # Check for optimized build flag
 if [ "$1" = 'v3' ]; then
@@ -28,8 +25,10 @@ fi
 # Build Torzu
 
 cd ./torzu
-COMM_HASH="$(git rev-parse --short HEAD)"
-VERSION="${COMM_HASH}"
+COUNT="$(git rev-list --count HEAD)"
+HASH="$(git rev-parse --short HEAD)"
+DATE="$(date +"%Y%m%d")"
+VERSION="${HASH}"
 git submodule update --init --recursive -j$(nproc)
 #Replaces 'boost::asio::io_service' with 'boost::asio::io_context' for compatibility with Boost.ASIO versions 1.74.0 and later
 find src -type f -name '*.cpp' -exec sed -i 's/boost::asio::io_service/boost::asio::io_context/g' {} \;
@@ -47,6 +46,7 @@ cmake .. -GNinja \
          -DYUZU_USE_BUNDLED_SDL2=ON \
  	 -DYUZU_USE_EXTERNAL_SDL2=OFF \
 	 -DYUZU_ENABLE_LTO=ON \
+  	 -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
 	 -DCMAKE_INSTALL_PREFIX=/usr \
 	 -DCMAKE_CXX_FLAGS="$ARCH_FLAGS -Wno-error" \
 	 -DCMAKE_C_FLAGS="$ARCH_FLAGS" \
@@ -56,10 +56,10 @@ ninja
 sudo ninja install
 echo "$VERSION" >~/version
 
-# NOW MAKE APPIMAGE, use AppImage-build.sh to generate target dir
+# NOW MAKE APPIMAGE, use AppImage-build-local.sh to generate target dir
 cd ..
-chmod +x ./AppImage-build.sh
-./AppImage-build.sh
+chmod +x ./AppImage-build-local.sh
+./AppImage-build-local.sh
 rm -rf ./torzu*.AppImage # Delete the generated appimage, cause it's useless now
 cp /usr/lib/libSDL3.so* ./AppImageBuilder/build/ # Copying libsdl3 to the already done appdir
 
@@ -67,6 +67,9 @@ cp /usr/lib/libSDL3.so* ./AppImageBuilder/build/ # Copying libsdl3 to the alread
 cd ..
 wget -q "$URUNTIME" -O ./uruntime
 chmod +x ./uruntime
+
+# Keep the mount point (speeds up launch time)
+sed -i 's|URUNTIME_MOUNT=[0-9]|URUNTIME_MOUNT=0|' ./uruntime
 
 #Add udpate info to runtime
 echo "Adding update information \"$UPINFO\" to runtime..."
@@ -81,7 +84,7 @@ echo "Generating AppImage..."
 	--no-history --no-create-timestamp \
 	--compression zstd:level=22 -S23 -B16 \
 	--header uruntime \
-	-i ./torzu/AppImageBuilder/build -o Torzu-"$VERSION"-Steamdeck-"$ARCH".AppImage
+	-i ./torzu/AppImageBuilder/build -o Torzu-"${DATE}"-"${COUNT}"-"${HASH}"-Steamdeck.AppImage
 
 echo "Generating zsync file..."
 zsyncmake *.AppImage -u *.AppImage
