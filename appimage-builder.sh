d05a2c2ce2be883b1112417da5a0591ace5029c3
Escary <81011361+pflyly@users.noreply.github.com>
2025-04-17 20:35:15 +0100

    Add files via upload

---
+++ appimage-builder.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# reg_server, 2024-01-09
+
+set -e
+
+# check arguments
+if [[ $# != 2 ]]; then
+    >&2 echo "Invalid arguments!"
+    echo "Usage: $0 torzu|sudachi <build dir>"
+    exit 1
+fi
+
+BUILD_APP="$1"
+if [[ "${BUILD_APP}" != "torzu" && "${BUILD_APP}" != "sudachi" ]]; then
+    >&2 echo "Invalid arguments!"
+    echo "Usage: $0 torzu|sudachi <build dir>"
+    exit 1
+fi
+
+BUILD_DIR=$(realpath "$2")
+if [[ ! -d "${BUILD_DIR}" ]]; then
+    >&2 echo "Invalid arguments!"
+    echo "'$2' is not a directory"
+    exit 1
+fi
+
+DEPLOY_LINUX_FOLDER="${BUILD_DIR}/deploy-linux"
+DEPLOY_LINUX_APPDIR_FOLDER="${BUILD_DIR}/deploy-linux/AppDir"
+BIN_FOLDER="${BUILD_DIR}/bin"
+BIN_EXE="${BIN_FOLDER}/${BUILD_APP//torzu/yuzu}"
+
+CPU_ARCH=$(uname -m)
+
+#export DISPLAYVERSION="1.2.3" # before cmake
+
+BIN_EXE_MIME_TYPE=$(file -b --mime-type "${BIN_EXE}")
+if [[ "${BIN_EXE_MIME_TYPE}" != "application/x-pie-executable" && "${BIN_EXE_MIME_TYPE}" != "application/x-executable" ]]; then
+    >&2 echo "Invalid or missing main executable (${BIN_EXE})!"
+    exit 1
+fi
+
+mkdir -p "${DEPLOY_LINUX_FOLDER}"
+rm -rf "${DEPLOY_LINUX_APPDIR_FOLDER}"
+
+cd "${BUILD_DIR}"
+
+# deploy/install to deploy-linux/AppDir
+DESTDIR="${DEPLOY_LINUX_APPDIR_FOLDER}" ninja install
+
+cd "${DEPLOY_LINUX_FOLDER}"
+
+dos2unix ./AppDir/usr/share/applications/org.sudachi_emu.sudachi.desktop
+
+curl -fsSLo ./linuxdeploy "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-${CPU_ARCH}.AppImage"
+chmod +x ./linuxdeploy
+
+curl -fsSLo ./linuxdeploy-plugin-qt "https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-${CPU_ARCH}.AppImage"
+chmod +x ./linuxdeploy-plugin-qt
+
+curl -fsSLo ./linuxdeploy-plugin-checkrt.sh https://github.com/darealshinji/linuxdeploy-plugin-checkrt/releases/download/continuous/linuxdeploy-plugin-checkrt.sh
+chmod +x ./linuxdeploy-plugin-checkrt.sh
+
+# Add Qt 6 specific environment variables
+export QT_QPA_PLATFORM="wayland;xcb"
+export EXTRA_PLATFORM_PLUGINS="libqwayland-egl.so;libqwayland-generic.so;libqxcb.so"
+export EXTRA_QT_PLUGINS="svg;wayland-decoration-client;wayland-graphics-integration-client;wayland-shell-integration;waylandcompositor;xcb-gl-integration;platformthemes/libqt6ct.so"
+
+NO_STRIP=1 APPIMAGE_EXTRACT_AND_RUN=1 ./linuxdeploy --appdir ./AppDir --plugin qt --plugin checkrt
+
+# remove libwayland-client because it has platform-dependent exports and breaks other OSes
+rm -fv ./AppDir/usr/lib/libwayland-client.so*
+
+# remove libvulkan because it causes issues with gamescope
+rm -fv ./AppDir/usr/lib/libvulkan.so*
+
+# Copying libsdl3 to the already done appdir
+cp /usr/lib/libSDL3.so* ./AppDir/usr/lib/ 
---
+++ sudachi-appimage.sh
@@ -0,0 +1,137 @@
+#!/bin/sh
+
+set -e
+
+export APPIMAGE_EXTRACT_AND_RUN=1
+export ARCH="$(uname -m)"
+export HOME=$(realpath "./")
+export pkgver="1.0.15"
+
+URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
+SUDACHI="https://github.com/emuplace/sudachi.emuplace.app/releases/download/v${pkgver}/latest.zip"
+
+echo "Making optimized build of sudachi"
+ARCH_FLAGS="-march=znver2 -mtune=znver2 -O3 -flto=auto"
+
+UPINFO="gh-releases-zsync|$(echo "$GITHUB_REPOSITORY" | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
+
+# BUILD SUDACHI
+if [ ! -d ./sudachi ]; then
+	wget -q "$SUDACHI"
+	mkdir ./sudachi
+	unzip latest.zip -d ./sudachi
+ 	cp -f ./CMakeLists.txt ./sudachi/src/sudachi_cmd/ #disable non-needed CMD file
+fi
+
+cd ./sudachi
+# List of submodule paths
+submodule_paths=(
+    "externals/enet"
+    "externals/dynarmic"
+    "externals/libusb/libusb"
+    "externals/discord-rpc"
+    "externals/vulkan-headers"
+    "externals/sirit"
+    "externals/mbedtls"
+    "externals/xbyak"
+    "externals/opus"
+    "externals/cpp-httplib"
+    "externals/ffmpeg/ffmpeg"
+    "externals/cpp-jwt"
+    "externals/libadrenotools"
+    "externals/VulkanMemoryAllocator"
+    "externals/breakpad"
+    "externals/simpleini"
+    "externals/oaknut"
+    "externals/Vulkan-Utility-Libraries"
+    "externals/vcpkg"
+    "externals/nx_tzdb/tzdb_to_nx"
+    "externals/cubeb"
+    "externals/SDL3"
+)
+
+for path in "${submodule_paths[@]}"; do
+    if [ -d "$path" ]; then
+        echo "Deleting existing folder: $path"
+        rm -rf "$path"
+    fi
+done
+
+git init
+
+git submodule add https://github.com/lsalzman/enet externals/enet
+git submodule add https://github.com/sudachi-emu/dynarmic externals/dynarmic
+git submodule add https://github.com/libusb/libusb externals/libusb/libusb
+git submodule add https://github.com/sudachi-emu/discord-rpc externals/discord-rpc
+git submodule add https://github.com/KhronosGroup/Vulkan-Headers externals/vulkan-headers
+git submodule add https://github.com/sudachi-emu/sirit externals/sirit
+git submodule add https://github.com/sudachi-emu/mbedtls externals/mbedtls
+git submodule add https://github.com/herumi/xbyak externals/xbyak
+git submodule add https://github.com/xiph/opus externals/opus
+git submodule add https://github.com/yhirose/cpp-httplib externals/cpp-httplib
+git submodule add https://github.com/FFmpeg/FFmpeg externals/ffmpeg/ffmpeg
+git submodule add https://github.com/arun11299/cpp-jwt externals/cpp-jwt
+git submodule add https://github.com/bylaws/libadrenotools externals/libadrenotools
+git submodule add https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator externals/VulkanMemoryAllocator
+git submodule add https://github.com/sudachi-emu/breakpad externals/breakpad
+git submodule add https://github.com/brofield/simpleini externals/simpleini
+git submodule add https://github.com/sudachi-emu/oaknut externals/oaknut
+git submodule add https://github.com/KhronosGroup/Vulkan-Utility-Libraries externals/Vulkan-Utility-Libraries
+git submodule add https://github.com/microsoft/vcpkg externals/vcpkg
+git submodule add https://github.com/lat9nq/tzdb_to_nx externals/nx_tzdb/tzdb_to_nx
+git submodule add https://github.com/mozilla/cubeb externals/cubeb
+git submodule add https://github.com/libsdl-org/sdl externals/SDL3
+
+git submodule update --init --recursive
+
+cd externals/cpp-httplib && git checkout 65ce51aed7f15e40e8fb6d2c0a8efb10bcb40126
+cd "${HOME}"/sudachi/externals/xbyak && git checkout v6.68
+
+cd "${HOME}"/sudachi
+mkdir build
+cd build
+cmake .. -GNinja \
+	 -DSUDACHI_USE_BUNDLED_VCPKG=ON \
+  	 -DENABLE_SDL3=ON \
+         -DENABLE_QT6=ON \
+	 -DSUDACHI_USE_QT_WEB_ENGINE=ON \
+	 -DSUDACHI_USE_BUNDLED_FFMPEG=ON \
+	 -DSUDACHI_TESTS=OFF \
+	 -DSUDACHI_CHECK_SUBMODULES=OFF \
+	 -DENABLE_QT_TRANSLATION=ON \
+	 -DSUDACHI_ENABLE_LTO=ON \
+  	 -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+	 -DCMAKE_INSTALL_PREFIX=/usr \
+	 -DCMAKE_CXX_FLAGS="$ARCH_FLAGS -Wno-error -w" \
+	 -DCMAKE_C_FLAGS="$ARCH_FLAGS" \
+	 -DCMAKE_SYSTEM_PROCESSOR="$(uname -m)" \
+	 -DCMAKE_BUILD_TYPE=Release
+ninja
+
+# use appimage-builder.sh to generate target dir
+cd "${HOME}"
+chmod +x ./appimage-builder.sh
+./appimage-builder.sh sudachi ./sudachi/build
+
+# turn appdir into appimage
+wget -q "$URUNTIME" -O ./uruntime
+chmod +x ./uruntime
+
+#Add udpate info to runtime
+echo "Adding update information \"$UPINFO\" to runtime..."
+printf "$UPINFO" > data.upd_info
+llvm-objcopy --update-section=.upd_info=data.upd_info \
+	--set-section-flags=.upd_info=noload,readonly ./uruntime
+printf 'AI\x02' | dd of=./uruntime bs=1 count=3 seek=8 conv=notrunc
+
+echo "Generating AppImage..."
+./uruntime --appimage-mkdwarfs -f \
+	--set-owner 0 --set-group 0 \
+	--no-history --no-create-timestamp \
+	--compression zstd:level=22 -S26 -B32 \
+	--header uruntime \
+	-i ./sudachi/build/deploy-linux/AppDir -o sudachi-"$VERSION"-steamdeck-"$ARCH".AppImage
+
+echo "Generating zsync file..."
+zsyncmake *.AppImage -u *.AppImage
+echo "All Done!"
