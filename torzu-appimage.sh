2923cee2236d122241d005a6886c0e3077df781c
Escary <81011361+pflyly@users.noreply.github.com>
2025-04-18 23:50:50 +0100

    switch to use appimage builder

--- torzu-appimage.sh
+++ torzu-appimage.sh
@@ -4,11 +4,11 @@ set -e
 
 export APPIMAGE_EXTRACT_AND_RUN=1
 export ARCH="$(uname -m)"
-LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
+
 URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
 
 echo "Making optimized build of torzu"
-ARCH_FLAGS="-march=znver2 -mtune=znver2 -O3 -ffast-math -flto=auto"
+ARCH_FLAGS="-march=znver2 -mtune=znver2 -O3 -flto=auto"
 
 UPINFO="gh-releases-zsync|$(echo "$GITHUB_REPOSITORY" | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
 
@@ -18,14 +18,14 @@ if [ ! -d ./torzu ]; then
 fi
 
 # Build Torzu
-
 cd ./torzu
 COUNT="$(git rev-list --count HEAD)"
 HASH="$(git rev-parse --short HEAD)"
 DATE="$(date +"%Y%m%d")"
 VERSION="${HASH}"
 git submodule update --init --recursive -j$(nproc)
-#Replaces 'boost::asio::io_service' with 'boost::asio::io_context' for compatibility with Boost.ASIO versions 1.74.0 and later
+
+# Replaces 'boost::asio::io_service' with 'boost::asio::io_context' for compatibility with Boost.ASIO versions 1.74.0 and later
 find src -type f -name '*.cpp' -exec sed -i 's/boost::asio::io_service/boost::asio::io_context/g' {} \;
 	
 mkdir build
@@ -51,15 +51,13 @@ ninja
 sudo ninja install
 echo "$VERSION" >~/version
 
-# NOW MAKE APPIMAGE, use AppImage-build-local.sh to generate target dir
+# use appimage-builder.sh to generate target dir
 cd ..
-chmod +x ./AppImage-build-local.sh
-./AppImage-build-local.sh
-rm -rf ./torzu*.AppImage # Delete the generated appimage, cause it's useless now
-cp /usr/lib/libSDL3.so* ./AppImageBuilder/build/ # Copying libsdl3 to the already done appdir
+cd ..
+chmod +x ./appimage-builder.sh
+./appimage-builder.sh torzu ./torzu/build
 
 # turn build dir into appimage
-cd ..
 wget -q "$URUNTIME" -O ./uruntime
 chmod +x ./uruntime
 
@@ -73,7 +71,7 @@ echo "Generating AppImage..."
 	--no-history --no-create-timestamp \
 	--compression zstd:level=22 -S26 -B32 \
 	--header uruntime \
-	-i ./torzu/AppImageBuilder/build -o Torzu-"${DATE}"-"${COUNT}"-"${HASH}"-Steamdeck.AppImage
+	-i ./torzu/build/deploy-linux/AppDir -o Torzu-"${DATE}"-"${COUNT}"-"${HASH}"-Steamdeck.AppImage
 
 echo "Generating zsync file..."
 zsyncmake *.AppImage -u *.AppImage
