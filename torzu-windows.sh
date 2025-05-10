d9b0621b453b2ca235ccaaab53ed2d8cb21ea2aa
Escary <81011361+pflyly@users.noreply.github.com>
2025-05-10 08:42:09 +0100

    Update torzu-windows.sh

--- torzu-windows.sh
+++ torzu-windows.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-git clone --branch libusb --single-branch https://github.com/pflyly/torzu-mirror.git ./torzu
+git clone https://github.com/pflyly/torzu-mirror.git ./torzu
 
 cd ./torzu
 git submodule update --init --recursive
